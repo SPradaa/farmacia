1766af7d3c5912d9c6ef5baee5581e8f16236c75
Juanxho24 <jeortega874@misena.edu.co>
2024-06-04 11:39:04 -0500

    Organizacion de carpetas para autorizacion

--- Model/admins/medico/modulomedico.php
+++ Model/admins/medico/modulomedico.php
@@ -279,6 +279,36 @@ validarSesion();
 
                                     <!-- fin modulo  laboratorios -->
 
+                                    
+                                    <!-- modulo historia clinica -->
+
+
+                                    <div class="card">
+
+                                    <a href="autorizaciones/index_automedicam.php">
+                                        <div class="card_box">
+                                    <h3 class="car_box__title">Citas
+                                    </h3>
+                                        <p class="card_box__content">Accede a la gestión de datos relacionados
+                                        con citas </p>
+                                            <div class="card__date">
+                                            Haz clic para verificar las citas agendadas.
+                                    </div>
+                                    <div class="card_box__arrow">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" height="15" width="15">
+                                        <path fill="#fff"
+                                        d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
+                                        </path>
+                                    </svg>
+                                    </div>
+                                    </div>
+                                    </a>
+                                    </div>
+
+                                    <!-- fin modulo  Historia clinica -->
+
+
                                     <!-- modulo autorizacion -->
 
 
@@ -304,34 +334,6 @@ validarSesion();
                                 </div>
                             </a>
 </div>
-
-                                    <!-- fin modulo  Autorización -->
-
-                                    <!-- modulo historia clinica -->
-
-
-                                    <div class="card">
-
-                                    <a href="histo_clinica/index_histoclinica.php">
-                                        <div class="card_box">
-                                    <h3 class="car_box__title">Historia Clínica
-                                    </h3>
-                                        <p class="card_box__content">Accede a la gestión de datos relacionados
-                                        con historia clinica </p>
-                                            <div class="card__date">
-                                            Haz clic para verificar la historia clínica correspondiente.
-                                    </div>
-                                    <div class="card_box__arrow">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                        viewBox="0 0 24 24" height="15" width="15">
-                                        <path fill="#fff"
-                                        d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z">
-                                        </path>
-                                    </svg>
-                                    </div>
-                                    </div>
-                                    </a>
-                                    </div>
 
                                     <!-- fin modulo  Autorización -->
 
